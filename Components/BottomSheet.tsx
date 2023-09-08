9aba8b8e3d469a68a31ad4a79c0dd3665f74644d
Musa Gillani <musagillaniwork@gmail.com>
2023-09-08 17:52:26 +0500

    Bottom sheet functionality

---
+++ Components/BottomSheet.tsx
@@ -0,0 +1,154 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import {
+  BottomSheetBackdrop,
+  BottomSheetBackgroundProps,
+  BottomSheetModal,
+  useBottomSheetModal,
+} from "@gorhom/bottom-sheet";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export type Ref = BottomSheetModal;
+
+const BottomSheet = React.forwardRef<Ref>((props, ref) => {
+  const snapPoints = React.useMemo(() => ["50%"], []);
+  const renderBackdrop = React.useCallback(
+    (props: BottomSheetBackgroundProps) => (
+      <BottomSheetBackdrop
+        {...props}
+        appearsOnIndex={0}
+        disappearsOnIndex={-1}
+      />
+    ),
+    []
+  );
+  const { dismiss } = useBottomSheetModal();
+  return (
+    <BottomSheetModal
+      ref={ref}
+      snapPoints={snapPoints}
+      overDragResistanceFactor={0}
+      backdropComponent={renderBackdrop}
+      backgroundStyle={{
+        backgroundColor: Colors.lightGrey,
+        borderRadius: 0,
+      }}
+      handleIndicatorStyle={{
+        display: "none",
+      }}
+    >
+      <View style={styles.contentContainer}>
+        <View style={styles.toggle}>
+          <TouchableOpacity style={styles.toggleActive}>
+            <Text style={styles.activeText}>Delivery</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.toggleInActive}>
+            <Text style={styles.InActiveText}>Pickup</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Text style={styles.subHeader}>Your Location</Text>
+        <Link href={"/"} asChild>
+          <TouchableOpacity>
+            <View style={styles.item}>
+              <Ionicons
+                name="location-outline"
+                size={20}
+                color={Colors.primary}
+              />
+              <Text style={{ flex: 1 }}>Use current location</Text>
+              <Ionicons
+                name="chevron-forward"
+                size={20}
+                color={Colors.primary}
+              />
+            </View>
+          </TouchableOpacity>
+        </Link>
+        <Text style={styles.subHeader}>Arrival Time</Text>
+
+        <Link href={"/"} asChild>
+          <TouchableOpacity>
+            <View style={styles.item}>
+              <Ionicons
+                name="stopwatch-outline"
+                size={20}
+                color={Colors.primary}
+              />
+              <Text style={{ flex: 1 }}>Now</Text>
+              <Ionicons
+                name="chevron-forward"
+                size={20}
+                color={Colors.primary}
+              />
+            </View>
+          </TouchableOpacity>
+        </Link>
+        <TouchableOpacity style={styles.button} onPress={() => dismiss()}>
+          <Text style={styles.buttonText}>Confirm</Text>
+        </TouchableOpacity>
+      </View>
+    </BottomSheetModal>
+  );
+});
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    flex: 1,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    padding: 16,
+    margin: 16,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+
+  toggle: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 10,
+    marginBottom: 32,
+  },
+  toggleActive: {
+    backgroundColor: Colors.primary,
+    padding: 8,
+    borderRadius: 32,
+    paddingHorizontal: 30,
+  },
+  activeText: {
+    color: "#fff",
+    fontWeight: "700",
+  },
+  InActiveText: {
+    color: Colors.primary,
+  },
+  toggleInActive: {
+    // backgroundColor: Colors.primary,
+    padding: 8,
+    borderRadius: 32,
+    paddingHorizontal: 30,
+  },
+  subHeader: {
+    fontSize: 16,
+    fontWeight: "600",
+    margin: 16,
+  },
+  item: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 16,
+    borderColor: Colors.grey,
+    borderWidth: 1,
+  },
+});
+export default BottomSheet;
--- Components/CustomHeader.tsx
+++ Components/CustomHeader.tsx
@@ -5,6 +5,8 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { Ionicons } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
 import { Link } from "expo-router";
+import BottomSheet from "./BottomSheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
 
 const SearchBar = () => (
   <View style={styles.searchContainer}>
@@ -32,24 +34,27 @@ const SearchBar = () => (
 
 const CustomHeader = () => {
   const inests = useSafeAreaInsets();
+  const bottomSheetRef = React.useRef<BottomSheetModal>(null);
+  const openModal = () => {
+    bottomSheetRef.current?.present();
+  };
   return (
     <View style={{ paddingTop: inests.top }}>
+      <BottomSheet ref={bottomSheetRef} />
       <View style={styles.container}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={openModal}>
           <Image
             style={styles.bike}
             source={require("@/assets/images/bike.png")}
           />
         </TouchableOpacity>
-
-        <TouchableOpacity style={styles.titleContainer}>
+        <TouchableOpacity style={styles.titleContainer} onPress={openModal}>
           <Text style={styles.title}>Delivery . Now</Text>
           <View style={{ flexDirection: "row", alignItems: "center" }}>
             <Text style={styles.subTitle}>San Francisco, CA </Text>
             <Ionicons name="chevron-down" size={20} color={Colors.primary} />
           </View>
         </TouchableOpacity>
-
         <TouchableOpacity style={styles.profileButton}>
           <Ionicons name="person-outline" size={20} color={Colors.primary} />
         </TouchableOpacity>
